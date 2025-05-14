ebc5c8d704185840e40e3f0c6006e4ec6d5fabcf
francisco_correia47 <jfranciscosacorreia@gmail.com>
2025-05-14 15:28:52 -0100

    Declaração das funções de menu
    Este ficheiro contém a declaração das funções de menu e também de macros utilizadas nessas funções.

--- menu.h
+++ menu.h
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+
+#define ALTURA  10
+#define LARGURA 55
+
+void menu_principal(Tarefa *tarefas, int qtd_de_tarefas);
+void menu_listar(Tarefa *tarefas, int qtd_de_tarefas);
