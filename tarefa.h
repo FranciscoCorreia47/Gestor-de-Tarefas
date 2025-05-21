352d8d05fc1ed3cb7ff99bab182f0a0319378f6a
PedroMiguel2708 <almeidapedro2708@gmail.com>
2025-05-21 22:24:48 -0100

    Update tarefa.h

--- tarefa.h
+++ tarefa.h
@@ -1,1 +1,18 @@
+#ifndef TAREFA_H
+#define TAREFA_H
 
+#define MAX_TAREFAS 100
+#define MAX_DESC 100
+
+typedef struct {
+    int id;
+    char descricao[MAX_DESC];
+    int concluida; // 0 = não, 1 = sim
+} Tarefa;
+
+void listar_tarefas(Tarefa *tarefas, int qtd);
+int adicionar_tarefa(Tarefa *tarefas, int *qtd, const char *descricao);
+int concluir_tarefa(Tarefa *tarefas, int qtd, int id);
+int remover_tarefa(Tarefa *tarefas, int *qtd, int id);
+
+#endif // TAREFA_H
