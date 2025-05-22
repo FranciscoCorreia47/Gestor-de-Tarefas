65e4371cfabae8934c51378d828ddbc98d16d84f
PedroMiguel2708 <almeidapedro2708@gmail.com>
2025-05-22 20:12:52 -0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include "tarefa.h"
+#include "arquivo.h"
+#include "menu.h"
+
+int main() {
+    Tarefa tarefas[MAX_TAREFAS]; //Cria um vetor (array) chamado tarefas, com capacidade máxima definida por MAX_TAREFAS.
+    int qtd = carregar_tarefas(tarefas, MAX_TAREFAS //Chama a função carregar_tarefas, que lê do arquivo as tarefas previamente salvas.
+    int opcao;
+    do {
+        mostrar_menu();// Chama a função mostrar_menu() para exibir as opções disponíveis ao usuário.
+        scanf("%d", &opcao);
+        executar_opcao(opcao, tarefas, &qtd);
+    } while (opcao != 6);
+    salvar_tarefas(tarefas, qtd); //Após o usuário sair do programa, as tarefas atuais são salvas no arquivo, garantindo que as alterações não sejam perdidas.
+
+    return 0;
+}
 
