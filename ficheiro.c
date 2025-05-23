efae72c8cacbc064e050b336957bda7d23adf20b
PedroMiguel2708 <almeidapedro2708@gmail.com>
2025-05-23 19:52:21 -0100

    Update ficheiro.c

--- ficheiro.c
+++ ficheiro.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "arquivo.h"
 
+// Função para salvar tarefas no arquivo texto
 int salvar_tarefas(Tarefa *tarefas, int qtd) {
     FILE *f = fopen(ARQUIVO_TAREFAS, "w");//Abre o arquivo definido por ARQUIVO_TAREFAS para escrita ("w").
     if (!f) return 0;//Se f for NULL, significa que o arquivo não pôde ser aberto;A função retorna 0 indicando falha.
@@ -13,9 +14,9 @@ int salvar_tarefas(Tarefa *tarefas, int qtd) {
 
     return 1;
 }
-
+// Função para carregar tarefas do arquivo texto
 int carregar_tarefas(Tarefa *tarefas, int max) {
-    FILE *f = fopen(ARQUIVO_TAREFAS, "r");//Abre o arquivo para leitura ("r").
+    FILE *f = fopen(ARQUIVO_TAREFAS, "r");//Abre o arquivo para leitura.
     if (!f) return 0; //Se o arquivo não existir ou não puder ser aberto, retorna 0.
     int qtd = 0;
     while (fscanf(f, "%d|%99[^|]|%d\n", &tarefas[qtd].id, tarefas[qtd].descricao, &tarefas[qtd].concluida) == 3 && qtd < max) {
