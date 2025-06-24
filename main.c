2cd78a369191c7f3f1b3bee74c8bacf6b3add247
PedroMiguel2708 <almeidapedro2708@gmail.com>
2025-06-24 11:38:05 -0100

    Update main.c

--- main.c
+++ main.c
@@ -2,15 +2,12 @@
 #include "tarefa.h"
 #include "ficheiro.h"
 
-
 int main() {
-  Tarefa tarefas[MAX_TAREFAS];
-  int qtd_de_tarefas = 0;
+  Tarefa tarefas[MAX_TAREFAS];    // Array que armazenará as tarefas em memória
+  int qtd_de_tarefas = 0;         // Número de tarefas atualmente carregadas
 
+  carregar_tarefas(tarefas, &qtd_de_tarefas);  // Lê as tarefas previamente guardadas no ficheiro (se houver)
+  menu_principal(tarefas, &qtd_de_tarefas);    // Inicia o menu interativo para o utilizador
 
-  carregar_tarefas(tarefas, &qtd_de_tarefas);
-  menu_principal(tarefas, &qtd_de_tarefas);
-
-
-  return 0;
+  return 0;  // Termina o programa com sucesso
 }
